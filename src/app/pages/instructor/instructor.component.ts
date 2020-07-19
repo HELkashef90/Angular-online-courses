e6b7c4c4298c1ce1c2435e4292ff38fc47f6269a
Hossam ELkashef <hossam.ahmed.elkashef@gmail.com>
2020-07-19 11:02:08 +0200

    create course UI update

--- src/app/pages/instructor/instructor.component.ts
+++ src/app/pages/instructor/instructor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LazyLoadScriptsService } from 'src/app/services/lazyLoadScripts/lazy-load-scripts.service';
 import { SpinnerService } from 'src/app/services/spinner/spinner.service';
+import { LocalizationService } from 'src/app/services/localization/localization.service';
 
 @Component({
   selector: 'app-instructor',
@@ -9,12 +10,16 @@ import { SpinnerService } from 'src/app/services/spinner/spinner.service';
 })
 export class InstructorComponent implements OnInit {
 
-  constructor(private _lazyLoadScript: LazyLoadScriptsService, private _spinnerService: SpinnerService) { }
+  constructor(private _lazyLoadScript: LazyLoadScriptsService, private _spinnerService: SpinnerService,
+    private _localizationService: LocalizationService) { }
 
   ngOnInit(): void {
 
   }
   ngAfterViewInit() {
+    // setTimeout(() => {
+    //   // this._localizationService.setUserLang('ar')
+    // }, 10000);
     this._spinnerService.showFullScreenSpinner()
     setTimeout(() => {
       this._spinnerService.hideFullScreenSpinner()
