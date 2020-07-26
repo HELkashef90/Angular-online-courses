a7f4ed0fb935a8b76e4e0efe9c69e00aa2dbdf4f
Hossam ELkashef <hossam.ahmed.elkashef@gmail.com>
2020-07-26 21:42:36 +0200

    admin signout

--- src/app/pages/admin/components/admin-sidebar/admin-sidebar.component.ts
+++ src/app/pages/admin/components/admin-sidebar/admin-sidebar.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from 'src/app/services/auth/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminSidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _auth: AuthService) { }
 
   ngOnInit(): void {
   }
-
+  signOut() {
+    this._auth.signOut()
+  }
 }
