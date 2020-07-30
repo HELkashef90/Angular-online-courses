959db0b08e5b85a77660a19ca2ed087656efe82e
Hossam ELkashef <hossam.ahmed.elkashef@gmail.com>
2020-07-30 16:38:17 +0200

    contact us ui

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../services/auth/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _auth : AuthService) { }
 
   ngOnInit(): void {
   }
-
+  goToDashboard(){
+    this._auth.redirectUserToDashboard()
+  }
 }
