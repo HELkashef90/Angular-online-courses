d964befc461bb3e97706736803842d448fc54d1f
Hossam <hossam.ahmed.elkashef@gmail.com>
2021-03-14 13:03:20 +0200

    quality conf

---
+++ src/assets/quality_conf/qualityConf.ts
@@ -0,0 +1,8 @@
+// Vimeo Plus, PRO, and Business members can default an embedded video to a specific quality on desktop.
+// Possible values: 4K, 2K, 1080p, 720p, 540p, 360p and 240p 
+// https://help.vimeo.com/hc/en-us/articles/224983008-Setting-default-quality-for-embedded-videos
+
+export const quality = {
+    mobileQuality: '360p',
+    desktopQuality: '360p'
+}
